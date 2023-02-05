9efb1c8c1dc190a4fad33f9276917ff4fcf5a7ae
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-02-05 19:30:36 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
-from web3 import Web3
-from termcolor import cprint
 import time
 import json
 import random
 from tqdm import tqdm
-import decimal
-import requests
-from tabulate import tabulate
-from decimal import Decimal
+
 
 # NOVA | ARBITRUM | OPTIMISM
 CHAIN = 'ARBITRUM'
